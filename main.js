aa9ba7f4095733d51c2a6992f06c4353cdff92fb
Claudia Martinez <clodos_m92@hotmail.com>
2021-08-21 15:08:50 +0200

    if statement hinzugefügt

--- main.js
+++ main.js
@@ -18,9 +18,13 @@ function addingToDo(){
 
     let item = document.createElement("p");
     item.classList.add("itemStyle");
+
+    //wenn Inputfield nicht leer ist wird eine Todo hinzugefügt
+    if (inputField.value !== ""){
     item.innerText = inputField.value;
     toDoContainer.appendChild(item);
     inputField.value = "";
+    } else alert("Füge eine Aufgabe ein");
 
     item.addEventListener("click", function(){
         item.style.textDecoration = "line-through";
