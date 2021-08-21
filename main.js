fb7d72fbaf0ee1470300f3d4d86c655cc0e902e6
Claudia Martinez <clodos_m92@hotmail.com>
2021-08-21 15:28:17 +0200

    Checkbox hinzugefügt

--- main.js
+++ main.js
@@ -15,22 +15,34 @@ addToDoButton.addEventListener("click", addingToDo);
 
 //functions
 function addingToDo(){
-
+    //erstellt ein To Do Item
     let item = document.createElement("p");
     item.classList.add("itemStyle");
+
+    //erstellt ein Input element des Types Checkbox inkl. ID
+    let check = document.createElement("input");
+    check.setAttribute("type", "checkbox");
+    check.setAttribute("id", "checkBoxId");
 
     //wenn Inputfield nicht leer ist wird eine Todo hinzugefügt
     if (inputField.value !== ""){
     item.innerText = inputField.value;
+    // toDo Container ist parent of item and item is parent uf check
     toDoContainer.appendChild(item);
+    item.appendChild(check);
     inputField.value = "";
     } else alert("Füge eine Aufgabe ein");
 
-    item.addEventListener("click", function(){
+    check.addEventListener("click", function(){
+
+        if(check.checked === true){
         item.style.textDecoration = "line-through";
+        } else item.style.textDecoration = "none";
     })
 
     item.addEventListener("dblclick", function(){
+        //remove item = Item + Check sind weg 
         toDoContainer.removeChild(item);
+        
     })
 }
